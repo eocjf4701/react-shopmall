4a3f128cae89196a4dbcb825e13fc55b33f2c025
parkdaechul <eocjf4701@gmail.com>
2023-03-30 22:29:43 +0900

    홈페이지 card list 만들기

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
   return (
     <div>
-      <img src="https://lp2.hm.com/hmgoepprod?set=source[/11/58/1158d8420b757157c728900673c10394d80e39be.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]" />
+      <img src={item?.img} />
       <div>Conscious choice</div>
-      <div>벨티드 트윌 코트</div>
-      <div>99,000원</div>
-      <div>신제품</div>
+      <div>{item?.title}</div>
+      <div>{item?.price}</div>
+      <div>{item?.new === true ? '신제품' : ''}</div>
     </div>
   )
 }
